c6cb3bbe01ba1614c020b13ce018f0e84b57da07
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-11 08:18:52 +0530

    Pocketmon: Add a function to add tags with configurable logging

--- pocketmon.py
+++ pocketmon.py
@@ -2,12 +2,37 @@
 Add reading time tags to each article on Pocket.
 """
 
+import logging
+
 import pocket
 
 from config import (
     consumer_key,
     access_token,
+    logging_level,
+    logging_handler,
 )
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging_level)
+logger.addHandler(logging_handler)
+
+
+def add_tag(item, tag):
+    """ Add a tag on an article. """
+
+    logger.debug('Tag "%s" on %s -- %s' % (tag, item.id, item.title))
+
+    api.send("""
+    [
+        {
+            "action"  : "tags_add",
+            "item_id" : "%s",
+            "tags"    : "%s"
+        }
+    ]
+    """ % (item.id, tag))
+
 
 
 if __name__ == '__main__':
